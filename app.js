2826f80d322d7095e567e50abfa07e1b270f0815
amanullahakhundzada <amanullahkhan9631@gmail.com>
2022-08-29 23:39:10 -0700

    promiss Ex

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  *
  * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
@@ -14,11 +15,42 @@ function getList() {
     }, 10);
   });
 }
+console.log(getList());
 
 // TODO: Handle the resolved or rejected states of the promise
-
+let err = document.querySelector("#error");
+let ul = document.querySelector("#list");
+// let promise=getList();
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
+function handleList(list){
+  // console.log(list);
+  list.forEach(hobbit => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    ul.appendChild(li);
+  });
+}
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
+function handleError(err){
+  console.log(err);
+  feedbaackP.textContent=err.message;
+}
+async function updateDOMList() {
+  try {
+    let list = await getList();
+    list.forEach((hobbit) => {
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+      ul.appendChild(li);
+    });
+  } catch (err) {
+    console.error(err);
+    feedbackP.textContent = err.message;
+  }
+}
+
+updateDOMList();
+
+
